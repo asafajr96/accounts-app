686eaf1437dda0f526863048dea120cb9c52c825
Asafa Makan'a <asafajr96@gmail.com>
2020-08-07 13:10:19 +0200

    login functionality

--- acc/urls.py
+++ acc/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-	path('', views.register, name="register"),
-	path('login/', views.login, name="login"),
+	path('', views.registerpage, name="register"),
+	path('login/', views.loginpage, name="login"),
 ]
--- acc/views.py
+++ acc/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
+
+from django.contrib.auth import authenticate, login, logout
 
 from .forms import CreateUserForm
 
 @csrf_exempt
-def register(request):
+def registerpage(request):
 	form = CreateUserForm()
 
 
@@ -14,12 +17,30 @@ def register(request):
 		form = CreateUserForm(request.POST)
 		if form.is_valid():
 			form.save()
+			user = form.cleaned_data.get("username") #pulls saved username from the form.
+			messages.success(request, "Account was created for " + user) #Saved user confirmation message.
+			return redirect("login")
 
 	context = {"form":form}
 	return render(request, "register.html", context)
 
 @csrf_exempt
-def login(request):
+def loginpage(request):
+
+	if request.method =="POST":
+		username = request.POST.get("username_txt")
+		password = request.POST.get("password_txt")
+
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			return redirect("register")
+		else:
+			messages.info(request, "Incorrect login details! Please try again.")
+			
+
+
 	context = {}
 	return render(request, "login.html", context)
 
